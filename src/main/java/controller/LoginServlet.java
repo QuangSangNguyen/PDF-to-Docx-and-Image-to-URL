518fb25b85713dde4a8ddcde2fa97fccf3652aca
_HarrySang_ <quangsang0709@gmail.com>
2023-12-12 12:07:44 +0700

    Điều chỉnh Login

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -65,25 +65,4 @@ public class LoginServlet extends HttpServlet {
 		}
 
 	}
-
-	protected void doPut(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String destination = null;
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-
-		UserBO checkLoginBO = new UserBO();
-		if (checkLoginBO.isValidUser(username, password)) {
-			destination = "/index.jsp";
-			request.getSession().setAttribute("username", username);
-			RequestDispatcher rd = getServletContext().getRequestDispatcher(destination);
-			rd.forward(request, response);
-		} else {
-			destination = "/login.jsp";
-			RequestDispatcher rd = getServletContext().getRequestDispatcher(destination);
-			rd.forward(request, response);
-		}
-
-	}
 }
